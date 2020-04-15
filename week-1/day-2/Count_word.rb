c49a6080a23d0509c7556a3014175071076ad47b
fernandinux <fernando.paca@gmail.com>
2020-04-15 01:31:07 -0500

    Assignment Count Word

---
+++ week-1/day-2/Count_word.rb
@@ -0,0 +1,10 @@
+class String
+    def count_words
+    f= Hash.new(0)
+    downcase.scan(/\w+/){|x| f[x]+=1}
+    return f
+    end
+
+    end
+    
+    puts "cake cake caKe I like cake very much please gIve me cake I need the cake i need it".count_words
